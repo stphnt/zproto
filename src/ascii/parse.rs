5f4c3c67eb3c7ef227dbd9f4b4016816329e3c2f
Stephen Hunt <stphnhnt.git@gmail.com>
2022-04-27 22:23:51 -0700

    Make ascii::parse module public

--- src/ascii/parse.rs
+++ src/ascii/parse.rs
@@ -13,31 +13,31 @@ pub use token::{RefTokens, Token, TokenIter, Tokens};
 pub use visitor::PacketKind;
 
 /// The byte preceding a checksum.
-pub const CHECKSUM_MARKER: u8 = b':';
+pub(crate) const CHECKSUM_MARKER: u8 = b':';
 
 /// The byte indicating the message is incomplete and continued in a subsequent info message.
-pub const MORE_PACKETS_MARKER: u8 = b'\\';
+pub(crate) const MORE_PACKETS_MARKER: u8 = b'\\';
 
 /// The first byte of a command message.
-pub const COMMAND_MARKER: u8 = b'/';
+pub(crate) const COMMAND_MARKER: u8 = b'/';
 
 /// The first byte of a reply message.
-pub const REPLY_MARKER: u8 = b'@';
+pub(crate) const REPLY_MARKER: u8 = b'@';
 
 /// The first byte of an info message.
-pub const INFO_MARKER: u8 = b'#';
+pub(crate) const INFO_MARKER: u8 = b'#';
 
 /// The first byte of an alert message.
-pub const ALERT_MARKER: u8 = b'!';
+pub(crate) const ALERT_MARKER: u8 = b'!';
 
 /// The carriage return byte.
-pub const CARRIAGE_RETURN: u8 = b'\r';
+pub(crate) const CARRIAGE_RETURN: u8 = b'\r';
 
 /// The line feed byte.
-pub const LINE_FEED: u8 = b'\n';
+pub(crate) const LINE_FEED: u8 = b'\n';
 
 /// A trait for identifying a byte as special characters in Zaber's ASCII protocol.
-pub trait AsciiExt {
+pub(crate) trait AsciiExt {
     /// The character is a reserved character
     fn is_reserved(&self) -> bool;
     /// The character is a word separator
@@ -86,7 +86,7 @@ impl AsciiExt for u8 {
 ///
 /// The contents of a message those after the packet start marker but before
 /// the checksum marker (`:`) or packet end marker;
-pub fn get_packet_contents(input: &[u8]) -> &[u8] {
+pub(crate) fn get_packet_contents(input: &[u8]) -> &[u8] {
     let start = input
         .iter()
         .position(|c| c.is_packet_start())
@@ -100,7 +100,7 @@ pub fn get_packet_contents(input: &[u8]) -> &[u8] {
 }
 
 /// A trait for parsing a type from a byte slice using `nom`.
-pub trait Nom: Sized {
+pub(crate) trait Nom: Sized {
     /// Parse an instance of `Self` from the `input` bytes.
     ///
     /// This conforms to `nom`'s parser interface and can be used with other
@@ -109,18 +109,18 @@ pub trait Nom: Sized {
 }
 
 /// Parse a `u8` from some bytes, assuming that it a number in base 10 and utf8.
-pub fn u8_from_base_10(input: &[u8]) -> Result<u8, ()> {
+pub(crate) fn u8_from_base_10(input: &[u8]) -> Result<u8, ()> {
     std::str::from_utf8(input)
         .map_err(|_| ())
         .and_then(|string| string.parse::<u8>().map_err(|_| ()))
 }
 
 /// Consume `input` until a reserved character is found
-pub fn take_till_reserved(input: &[u8]) -> IResult<&[u8], &[u8]> {
+pub(crate) fn take_till_reserved(input: &[u8]) -> IResult<&[u8], &[u8]> {
     take_till(|byte: u8| byte.is_reserved())(input)
 }
 
 /// Consume `input` until a tab, space, or reserved character is found.
-pub fn take_till_tab_space_reserved(input: &[u8]) -> IResult<&[u8], &[u8]> {
+pub(crate) fn take_till_tab_space_reserved(input: &[u8]) -> IResult<&[u8], &[u8]> {
     take_till(|byte: u8| byte == b' ' || byte == b'\t' || byte.is_reserved())(input)
 }
