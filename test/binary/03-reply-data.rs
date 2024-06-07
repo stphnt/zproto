10d4d67166f9d2737f18db1d7552457d19d1f076
Stephen Hunt <stphnhnt.git@gmail.com>
2024-06-06 18:25:09 -0700

    Update expected error for binary compile-time tests

--- test/binary/03-reply-data.rs
+++ test/binary/03-reply-data.rs
@@ -11,8 +11,8 @@ fn _fail<B: Backend>(port: &mut Port<B>) -> Result<(), Box<dyn std::error::Error
 
 fn _ok<B: Backend>(port: &mut Port<B>) -> Result<(), Box<dyn std::error::Error>> {
     let reply = port.tx_recv((0, MOVE_ABSOLUTE, 1000))?;
-    let value = reply.data()?;  // Shouldn't need any type hints
-    let value: bool = reply.to_untyped().data()?; // Although incorrect, this is not a compile-time error.
+    let _value = reply.data()?;  // Shouldn't need any type hints
+    let _value: bool = reply.to_untyped().data()?; // Although incorrect, this is not a compile-time error.
     Ok(())
 }
 
