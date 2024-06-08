7f57b28143b8a5499bb02903ca80db4d438876ef
Stephen Hunt <stphnhnt.git@gmail.com>
2024-06-07 20:03:21 -0700

    Document binary::Port::try_into_send

--- src/binary.rs
+++ src/binary.rs
@@ -105,6 +105,23 @@
 //! # }
 //! ```
 //!
+//! ## Sending a Port between threads
+//!
+//! By default a [`Port`] does not implement `Send`, so cannot be sent to another
+//! thread. If you're application requires this, use the [`Port::try_into_send`]
+//! method to convert it to one that can be. Doing so places [`Send`] bounds on
+//! any [packet](Port::set_packet_handler) handlers.
+//!
+//! ```
+//! # use zproto::ascii::Port;
+//! # fn wrapper() -> Result<(), Box<dyn std::error::Error>> {
+//! let sendable_port = Port::open_serial("...")?
+//!     .try_into_send()
+//!     .unwrap();
+//! # Ok(())
+//! # }
+//! ```
+//!
 //! ## Type Safety
 //!
 //! The library uses Rust's type system and the strongly-typed commands in the
